ed759bdd1147f1a7bc6a4d1030fc623c47d63192
dineth88 <dinethoshitha00@gmail.com>
2024-09-14 03:19:46 +0530

    Created file structure

---
+++ src/home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Button } from './components/ui/button'
+import {SignInButton} from '@clerk/clerk-react';
+import Header from './components/Header';
+
+function Home() {
+  return (
+    <>
+    <div>
+        <Header />
+    </div>
+    </>
+  )
+}
+
+export default Home
---
+++ src/main.jsx
@@ -0,0 +1,32 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import Home from './home'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import Contact from './contact'
+import {ClerkProvider} from '@clerk/clerk-react';
+
+const router = createBrowserRouter([
+  {
+    path:'/',
+    element:<Home/>
+  },
+  {
+    path:'/contact',
+    element:<Contact/>
+  }
+])
+
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error("Missing Publishable Key")
+}
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
+      <RouterProvider router={router} />
+    </ClerkProvider>
+  </StrictMode>,
+)
